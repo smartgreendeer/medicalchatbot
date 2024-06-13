54530eeb7eb5dd07a2ac3480f2f26fdf1b9c70cf
smartgreendeer <johnmuriithi7818@gmail.com>
2024-06-13 11:49:47 +0300

    change 1

---
+++ setup.py
@@ -0,0 +1,9 @@
+fromsetuptools import find_packages, setup
+setup(
+    name = 'Medical Chatbot Project'
+    Version = '0.0.1'
+    author = 'John Muriithi'
+    author_email = 'Johnmuriithi7818@gmail.com'
+    packages = 'find_packages()'
+    instal_requires = []
+)
